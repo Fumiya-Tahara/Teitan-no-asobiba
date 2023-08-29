0027a99c3c3ab0ab59f96353c51350a966dcb85e
nakashimashin <nakashimashin0517@gmail.com>
2023-08-29 16:18:36 +0900

    ゲームの説明文作成

---
+++ front/src/app/typingGame/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Discription from "./components/Discription"
+
+export default function Typing() {
+    return (
+      <>
+        <Discription />
+        <div className="">typing</div>
+      </>
+    )
+  }
+  
