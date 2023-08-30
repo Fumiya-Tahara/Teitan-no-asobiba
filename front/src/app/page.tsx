4cbdd610b00337a09be8ea294eb6724993a58bcb
nakashimashin <nakashimashin0517@gmail.com>
2023-08-30 13:42:18 +0900

    header追加

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import localImage from "./38-.jpg";
+import localImage2 from "./typingGame/components/W_Teitan_Black-teitan_Design_Manual_2016_004.jpg"
 import Link from "next/link";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <div className="h-screen bg-lime-300">
         <div className="flex flex-row justify-evenly items-center">
-          <Image className="w-[500px] h-[340px] rounded-lg mt-[95px]" src={localImage} alt="画像"/>
-          <div className="flex flex-col w-[500px] h-[300px] rounded-lg mt-[50px]">
+          <div className="flex flex-col justify-center items-center">
+            <Image className="w-[500px] h-[340px] rounded-lg mt-[180px]" src={localImage} alt="画像"/>
+            <Image className="w-[180px] h-[20px] rounded-lg mt-[-50px]" src={localImage2} alt="画像"/>
+          </div>
+          <div className="flex flex-col w-[500px] h-[300px] rounded-lg mt-[145px]">
             <p className="mb-[30px] text-[40px]">こんにちは！</p>
             <p className="text-2xl pt-[15px]">ボクたちはていたんとブラックていたん</p>
             <p className="text-2xl pt-[15px]">北九州市の人たちにもっと環境意識を持ってもらうために北極からやってきたんだ！</p>
